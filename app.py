f0a1c622543697c58f284c55b03e2f92daa09dfc
pramuditodhoni <biz.dhoni@gmail.com>
2020-05-06 23:07:43 +0700

    First commit templates 2

--- app.py
+++ app.py
@@ -16,14 +16,17 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
