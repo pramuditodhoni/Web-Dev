6f3921274d7a1143b947e77819bfe264862370fc
pramuditodhoni <biz.dhoni@gmail.com>
2020-05-09 23:41:14 +0700

    Making Form and Validation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '210a117f56e7b716bf75c34703fd897f'
+
 posts = [
     {
         'author': 'Dhoni Pramudito',
