74cd81c21f71786cb5f5e0715fa1d2fe64d3d1b1
pramuditodhoni <biz.dhoni@gmail.com>
2020-05-11 23:27:49 +0700

    Making Form and Validation 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '210a117f56e7b716bf75c34703fd897f'
@@ -28,6 +29,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm
+    render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm
+    render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
