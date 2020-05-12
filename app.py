edb6e3c7af1738b69fb6ce1b8b61b4cf45e29d84
pramuditodhoni <biz.dhoni@gmail.com>
2020-05-12 21:45:27 +0700

    Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -31,16 +31,19 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm
-    render_template('register.html', title='Register', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login')
 def login():
-    form = LoginForm
-    render_template('login.html', title='Login', form=form)
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
