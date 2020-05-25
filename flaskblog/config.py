da751fae6c793c10585f33e1389bea297fa4e54a
pramuditodhoni <biz.dhoni@gmail.com>
2020-05-25 20:21:31 +0700

    Blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '210a117f56e7b716bf75c34703fd897f'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
